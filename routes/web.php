fa91b9718d8846c6dc4d125ca55bcd41ca559f3c
adityailham193@gmail.com <adityailham193@gmail.com>
2024-05-21 00:06:41 +0700

    Edit route

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,18 @@
 <?php
 
+use App\Http\Controllers\Authentication\LoginController;
+use App\Http\Controllers\Authentication\LogoutController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('authentication.login');
-})->name('login');
+Route::middleware('guest')->group(function () {
+    Route::get('/', fn () => redirect()->route('login'));
 
-Route::get('/logout', function () {
-    return view('authentication.login');
-})->name('logout');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate']);
+});
 
-Route::get('/dashboard', function () {
-    return view('administrator.dashboard');
-})->name('dashboard');
+Route::post('/logout', LogoutController::class)->name('logout');
 
-Route::get('/administrator-commodity', function () {
-    return view('administrator.commodity.index');
-})->name('administrator-commodity');
-
-Route::get('/administrator-programstudy', function () {
-    return view('administrator.program_study.index');
-})->name('administrator-programstudy');
-
-Route::get('/administrator-class', function () {
-    return view('administrator.school_class.index');
-})->name('administrator-class');
-
-Route::get('/administrator-subject', function () {
-    return view('administrator.subject.index');
-})->name('administrator-subject');
-
-Route::get('/administrator-history', function () {
-    return view('administrator.borrowing.history.index');
-})->name('administrator-history');
-
-Route::get('/administrator-main', function () {
-    return view('administrator.borrowing.main.index');
-})->name('administrator-main');
-
-Route::get('/administrator-student', function () {
-    return view('administrator.student.index');
-})->name('administrator-student');
-
-Route::get('/administrator-admin', function () {
-    return view('administrator.user.index');
-})->name('administrator-admin');
-
+require_once __DIR__.'/administrator.php';
+require_once __DIR__.'/officer.php';
+require_once __DIR__.'/student.php';
